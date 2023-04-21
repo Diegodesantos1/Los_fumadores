8cc95910f49a3c2a1f3d42ec71b91578ecf3a002
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2023-04-21 15:20:13 +0200

    creado un diccionario y cambio en print

--- fumadores_completo.py
+++ fumadores_completo.py
@@ -5,11 +5,7 @@ from colorama import Fore, Back, Style
 
 
 def ejecutar():
-    papel = 0
-    tabaco = 1
-    filtros = 2
-    green = 3
-    cerillas = 4
+    diccionario = {0: "papel", 1: "tabaco", 2: "filtros", 3: "green", 4: "cerillas"}
     num_fumadores = 5
     ingredientes_necesarios = 5
     mutex = threading.Lock()
@@ -21,7 +17,7 @@ def ejecutar():
     def poner_ingredientes():
         while not terminar_evento.is_set():
             ing1, ing2 = random.sample(range(ingredientes_necesarios), 2)
-            print(f"Agente pone {ing1} y {ing2} en la mesa")
+            print(f"Fumador pone {diccionario[ing1]} y {diccionario[ing2]}")
             ingredientes[ing1] = True
             ingredientes[ing2] = True
             turno.clear()
