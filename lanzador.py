af576c85e0dd9a74a306632f278ead1297fa0b7c
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2023-04-21 15:09:44 +0200

    creado el lanzador

---
+++ lanzador.py
@@ -0,0 +1,15 @@
+
+import fumadores_completo as fumadores
+import fumadores_simple as fumadores_simple
+from introducir.numero import solicitar_introducir_numero_extremo
+
+def ejecutar():
+    eleccion = solicitar_introducir_numero_extremo("\n¿Qué versión quieres ejecutar? \n1: Completa \n2: Simplicada \n3: Terminar\n", 1, 3)
+    if eleccion == 1:
+        fumadores.ejecutar()
+        ejecutar()
+    elif eleccion == 2:
+        fumadores_simple.ejecutar()
+        ejecutar()
+    elif eleccion == 3:
+        exit()
