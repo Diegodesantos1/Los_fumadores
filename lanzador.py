b1feaf9590a7b1fffc2042958a22b71768f5851b
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2023-04-21 15:09:56 +0200

    puesto en formato pep8

--- lanzador.py
+++ lanzador.py
@@ -3,8 +3,10 @@ import fumadores_completo as fumadores
 import fumadores_simple as fumadores_simple
 from introducir.numero import solicitar_introducir_numero_extremo
 
+
 def ejecutar():
-    eleccion = solicitar_introducir_numero_extremo("\n¿Qué versión quieres ejecutar? \n1: Completa \n2: Simplicada \n3: Terminar\n", 1, 3)
+    eleccion = solicitar_introducir_numero_extremo(
+        "\n¿Qué versión quieres ejecutar? \n1: Completa \n2: Simplicada \n3: Terminar\n", 1, 3)
     if eleccion == 1:
         fumadores.ejecutar()
         ejecutar()
